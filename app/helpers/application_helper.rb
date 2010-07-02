cfa8335aa871002d691974411aaccc7ccd716d25
Julio Teruya <julio.teruya@gmail.com>
2010-07-02 16:54:03 -0500

    Correcciones en el check_timeout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def check_timeout
-    if ((Time.now - session[:time]) > 25.minutes)
+    if (session[:time])
+      if ((Time.now - session[:time]) > 25.minutes)
+        reset_session
+      end
+    else
       reset_session
     end
+    
   end
 end
